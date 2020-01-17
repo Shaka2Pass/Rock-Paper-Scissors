628a8799890a1ea6e28c8cd83615a24392f8353e
Will G Piro <willgpiro@gmail.com>
2020-01-17 11:30:41 -0800

    Updated RPS with changes from Lab 4

--- r-p-s.js
+++ r-p-s.js
@@ -1,13 +1,13 @@
-import getRandomThrow, { resultsOfThrow } from './get-random-throw.js';
+import { getRandomThrow, resultsOfThrow } from './get-random-throw.js';
 
 const attackButton = document.getElementById('attackButton');
-const winsDisplay = document.getElementById('wins');
+const winsDisplay = document.getElementById('win');
 const lossesDisplay = document.getElementById('losses');
 const drawsDisplay = document.getElementById('draws');
 const resultsDisplay = document.getElementById('attackResults');
 
 
-let wins = 0;
+let win = 0;
 let losses = 0;
 let draws = 0;
 
@@ -19,32 +19,26 @@ attackButton.addEventListener('click', () => {
 
     const gameResults = resultsOfThrow(usersChoice, computerChoice);
 
-    if (usersChoice === computerChoice) return 'draw';
-    if (usersChoice === 'rock' && computerChoice === 'scissors') return 'win';
-    if (usersChoice === 'scissors' && computerChoice === 'paper') return 'win';
-    if (usersChoice === 'paper' && computerChoice === 'rock') return 'win';
-   
 
-
-    if (gameResults === 'win')
-    {
-        wins ++;
-        winsDisplay.textContent = wins;
+    if (gameResults === 'win') {
+        win ++;
+        winsDisplay.textContent = win;
         resultsDisplay.textContent = 'YOU WIN!';
-
+        console.log('win');
     } 
 
     else if (gameResults === 'loss'){
         losses++;
         lossesDisplay.textContent = losses;
-        resultsDisplay.textContent = 'YOU LOSE SUCKA!';
+        resultsDisplay.textContent = 'YOU LOSE!';
+        console.log('loss');
 
     } else {
         draws++;
         drawsDisplay.textContent = draws; 
         resultsDisplay.textContent = 'DRAW!';
-    
+        console.log('draw');
+        
     }
+
 });
-
-
